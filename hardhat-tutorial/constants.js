8cf86d2f925f02f9550ca6a5bf113fbc33543266
Code <code@Decebals-MacBook-Air.local>
2022-12-03 20:49:58 +0100

    First commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xacaD26dDECAF3BDe03EF2d74067840D618e32c68";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
